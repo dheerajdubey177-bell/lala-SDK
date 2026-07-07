70e076618c1a0498125c37968b7e0a17cdeb7b1f
Dheeraj <dheerajdubey177@gmail.com>
2026-07-08 01:16:23 +0530

    Fix Pyinstaller entrypoint

--- build_sdk.bat
+++ build_sdk.bat
@@ -5,15 +5,15 @@ REM Install pyinstaller if not present
 pip install pyinstaller
 
 REM Build the compiler executable
-python -m PyInstaller --onefile bin\lala_compiler.py
+python -m PyInstaller --onefile tools\lala.py
 
 REM Copy the executable into the bin folder
-copy dist\lala_compiler.exe bin\lala.exe /Y
+copy dist\lala.exe bin\lala.exe /Y
 
 REM Cleanup pyinstaller build folders
 rmdir /s /q build
 rmdir /s /q dist
-del lala_compiler.spec
+del lala.spec
 
 echo Build complete! Your SDK is ready in the bin/ folder.
 echo You can zip the "bin", "core", and "templates" folders to distribute!
